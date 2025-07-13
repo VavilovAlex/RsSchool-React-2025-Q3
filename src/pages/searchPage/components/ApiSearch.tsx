b802f8c71f116617bea0c0b992b12250bfaf5512
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-13 15:41:38 +1000

    feat: implement ApiSearch.tsx

--- src/pages/searchPage/components/ApiSearch.tsx
+++ src/pages/searchPage/components/ApiSearch.tsx
@@ -1,13 +1,54 @@
-import { Component } from "react";
-import Button from "../../../components/button/Button.tsx";
+import { type ChangeEvent, Component } from "react";
 import TextInput from "../../../components/textInput/TextInput.tsx";
+import AsyncButton from "../../../components/button/AsyncButton.tsx";
+import { searchBooks } from "../../../api/book/client.ts";
+import type { PaginationOptions } from "../../../shared/types/pagination.ts";
+import type { BookSearchResponse } from "../../../api/book/models.ts";
 
-export default class ApiSearch extends Component {
+interface Props {
+  onSearchSuccess: (response: BookSearchResponse) => void;
+}
+
+interface State {
+  searchText: string;
+  pagination: PaginationOptions;
+}
+
+export default class ApiSearch extends Component<Props> {
+  state: State = {
+    searchText: localStorage.getItem("apiSearchText") || "",
+    pagination: {
+      page: 1,
+      pageSize: 10,
+    },
+  };
+
+  handleSearchTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    this.setState({ searchText: value });
+    localStorage.setItem("apiSearchText", value);
+  };
+
+  search = async () => {
+    const { searchText, pagination } = this.state;
+
+    const result = await searchBooks(searchText, pagination);
+
+    this.props.onSearchSuccess(result);
+  };
+
   render() {
+    const { searchText } = this.state;
+
     return (
       <div className="flex flex-row w-full gap-1">
-        <TextInput className={"w-full"} onChange={() => {}} />
-        <Button>Search</Button>
+        <TextInput
+          className={"w-full"}
+          value={searchText}
+          onChange={this.handleSearchTextChange}
+        />
+        <AsyncButton onClick={this.search}>Search</AsyncButton>
       </div>
     );
   }
