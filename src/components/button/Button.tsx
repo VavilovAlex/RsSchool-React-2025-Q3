41c20a23236e1eb21a7d59a350a1255da497111c
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-13 15:30:16 +1000

    refactor: simplify Button props by using ButtonHTMLAttributes

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,25 +1,15 @@
-import { type ReactNode, Component } from "react";
+import { Component, type ButtonHTMLAttributes } from "react";
 import { clsx } from "clsx";
-import type BaseComponentProps from "../../interfaces/base-component-props.ts";
 
-interface ButtonProps extends BaseComponentProps {
-  children: ReactNode;
-  onClick?: () => void;
-}
-
-class Button extends Component<ButtonProps> {
+class Button extends Component<ButtonHTMLAttributes<HTMLButtonElement>> {
   render() {
-    const { children, onClick, className, style } = this.props;
+    const { children, className, ...rest } = this.props;
 
     const baseClassName =
       "text-white bg-blue-500 p-2 rounded cursor-pointer hover:bg-blue-600 active:bg-blue-700";
 
     return (
-      <button
-        className={clsx(baseClassName, className)}
-        style={style}
-        onClick={onClick}
-      >
+      <button className={clsx(baseClassName, className)} {...rest}>
         {children}
       </button>
     );
