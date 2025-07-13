74d34383f03e64cf164811bf608ff71d3eb0d06e
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-13 15:57:01 +1000

    feat: implement ApiResults

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -2,17 +2,32 @@ import { Component } from "react";
 import ApiSearch from "./components/ApiSearch.tsx";
 import ApiResults from "./components/ApiResults.tsx";
 import Section from "./components/Section.tsx";
+import type { BookSearchResponse } from "../../api/book/models.ts";
+
+interface State {
+  response: BookSearchResponse | null;
+}
 
 export default class SearchPage extends Component {
+  state: State = {
+    response: null,
+  };
+
+  handleSearchSuccess = (response: BookSearchResponse) => {
+    this.setState({ response });
+  };
+
   render() {
+    const { response } = this.state;
+
     return (
       <div className={"flex flex-col w-full h-full p-4 items-center"}>
-        <div className={"max-w-[600px] w-full flex flex-col gap-4"}>
+        <div className={"max-w-[1200px] w-full flex flex-col gap-4"}>
           <Section title={"Search"}>
-            <ApiSearch />
+            <ApiSearch onSearchSuccess={this.handleSearchSuccess} />
           </Section>
           <Section title={"Results"}>
-            <ApiResults />
+            <ApiResults result={response} />
           </Section>
         </div>
       </div>
--- src/pages/searchPage/components/ApiResults.tsx
+++ src/pages/searchPage/components/ApiResults.tsx
@@ -1,25 +1,64 @@
 import { Component } from "react";
+import type { BookSearchResponse } from "../../../api/book/models.ts";
 
-class ApiResults extends Component {
+interface Props {
+  result: BookSearchResponse | null;
+}
+
+class ApiResults extends Component<Props> {
   render() {
+    const { result } = this.props;
+
     return (
       <div>
         <table className={"default-table"}>
           <thead>
             <tr>
               <th>Name</th>
-              <th>Description</th>
+              <th>Year</th>
+              <th>Authors</th>
+              <th>Open library</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>[Item 1]</td>
-              <td>[Description1]</td>
-            </tr>
-            <tr>
-              <td>[Item 2]</td>
-              <td>[Description2]</td>
-            </tr>
+            {result == null ? (
+              <tr>
+                <td colSpan={4} className={"italic"}>
+                  Search results will appear here
+                </td>
+              </tr>
+            ) : (
+              <>
+                {result.books.length === 0 && (
+                  <tr>
+                    <td colSpan={2} className={"italic"}>
+                      No results found.
+                    </td>
+                  </tr>
+                )}
+                {result.books.map((book) => (
+                  <tr key={book.key}>
+                    <td>{book.title}</td>
+                    <td>{book.firstPublishYear}</td>
+                    <td>
+                      {book.authors.map((author) => author.name).join(", ")}
+                    </td>
+                    <td>
+                      <a
+                        href={"https://openlibrary.org/" + book.key}
+                        target={"_blank"}
+                        rel="noreferrer"
+                        className={
+                          "text-blue-500 hover:underline cursor-pointer hover:text-blue-700 active:text-blue-900 visited:text-purple-600"
+                        }
+                      >
+                        Link
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            )}
           </tbody>
         </table>
       </div>
