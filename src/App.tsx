bf80b6cefc54e9ffd374d54fab862b6959a3426b
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-20 15:58:41 +1000

    chore: rename errorBoundary.tsx to start with a capital letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import SearchPage from "./pages/searchPage/SearchPage.tsx";
-import ErrorBoundary from "./components/errorBoundary/errorBoundary.tsx";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary.tsx";
 
 class App extends Component {
   render() {
