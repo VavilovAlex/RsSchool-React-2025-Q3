4110fddaefe2302cfc7c0f735adb164ad647741c
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-26 21:11:57 +1000

    refactor: convert SearchPage and App components to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import { Component } from "react";
 import SearchPage from "./pages/searchPage/SearchPage.tsx";
 import ErrorBoundary from "./components/errorBoundary/ErrorBoundary.tsx";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <ErrorBoundary>
-          <SearchPage />
-        </ErrorBoundary>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <ErrorBoundary>
+        <SearchPage />
+      </ErrorBoundary>
+    </>
+  );
 }
-
-export default App;
--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,72 +1,60 @@
-import { Component } from "react";
 import ApiSearch from "./components/ApiSearch.tsx";
 import ApiResults from "./components/ApiResults.tsx";
 import Section from "./components/Section.tsx";
 import type { BookSearchResponse } from "../../api/book/models.ts";
 import Button from "../../components/button/Button.tsx";
 import type { HttpError } from "../../shared/errors/httpError.ts";
+import { useState } from "react";
 
-interface State {
-  response: BookSearchResponse | null;
-  errorToThrow: Error | null;
-  searchError?: Error | null;
-}
+export default function SearchPage() {
+  const [response, setResponse] = useState<BookSearchResponse | null>(null);
+  const [errorToThrow, setErrorToThrow] = useState<Error | null>(null);
+  const [searchError, setSearchError] = useState<Error | null>(null);
 
-export default class SearchPage extends Component<object, State> {
-  state: State = {
-    response: null,
-    errorToThrow: null,
+  const handleSearchSuccess = (response: BookSearchResponse) => {
+    setResponse(response);
+    setSearchError(null);
   };
 
-  handleSearchSuccess = (response: BookSearchResponse) => {
-    this.setState({ response, searchError: null });
+  const handleSearchStart = () => {
+    setResponse(null);
   };
 
-  handleSearchStart = () => {
-    this.setState({ response: null });
+  const handleSearchError = (error: Error) => {
+    setSearchError(error);
+    setResponse({ numFound: 0, start: 0, books: [] });
   };
 
-  handleSearchError = (error: Error) => {
-    this.setState({
-      searchError: error,
-      response: { numFound: 0, start: 0, books: [] },
-    });
+  const throwTest = () => {
+    setErrorToThrow(new Error("Test error"));
   };
 
-  throw = () => {
-    this.setState({ errorToThrow: new Error("Test error") });
-  };
+  if (errorToThrow != null) throw errorToThrow;
 
-  render() {
-    const { response, searchError } = this.state;
+  const httpError = searchError as HttpError;
 
-    if (this.state.errorToThrow != null) throw this.state.errorToThrow;
-
-    const httpError = searchError as HttpError;
-
-    return (
-      <div className={"flex flex-col items-center w-full h-full p-4"}>
-        <div className={"flex flex-col gap-4 max-w-[1200px] w-full"}>
-          <Section title={"Search"}>
-            <ApiSearch
-              onSearchStart={this.handleSearchStart}
-              onSearchSuccess={this.handleSearchSuccess}
-              onSearchError={this.handleSearchError}
-            />
+  return (
+    <div className={"flex flex-col items-center w-full h-full p-4"}>
+      <div className={"flex flex-col gap-4 max-w-[1200px] w-full"}>
+        <Section title={"Search"}>
+          <ApiSearch
+            onSearchStart={handleSearchStart}
+            onSearchSuccess={handleSearchSuccess}
+            onSearchError={handleSearchError}
+          />
+        </Section>
+        {searchError && (
+          <Section title={"Search error"} className={"bg-red-100"}>
+            Search failed with code: {httpError.statusCode}
           </Section>
-          {searchError && (
-            <Section title={"Search error"} className={"bg-red-100"}>
-              Search failed with code: {httpError.statusCode}
-            </Section>
-          )}
-          <Section title={"Results"}>
-            <ApiResults result={response} />
-          </Section>
-          <Section title={"Test"}>
-            <Button onClick={this.throw}>Trigger Error Boundary</Button>
-          </Section>
-        </div>
+        )}
+        <Section title={"Results"}>
+          <ApiResults result={response} />
+        </Section>
+        <Section title={"Test"}>
+          <Button onClick={throwTest}>Trigger Error Boundary</Button>
+        </Section>
       </div>
-    );
-  }
+    </div>
+  );
 }
