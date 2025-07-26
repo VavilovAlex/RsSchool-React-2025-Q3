ef9b70d452fae4797ce99e5bb5116fe51825d4b1
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-26 22:07:15 +1000

    feat: integrate React Router and add NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import SearchPage from "./pages/searchPage/SearchPage.tsx";
 import ErrorBoundary from "./components/errorBoundary/ErrorBoundary.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import NotFound from "./pages/notFound/NotFound.tsx";
 
 export default function App() {
   return (
     <>
       <ErrorBoundary>
-        <SearchPage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </ErrorBoundary>
     </>
   );
