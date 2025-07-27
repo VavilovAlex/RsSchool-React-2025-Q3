0ba658799de0627e20164a63ea0003368206012c
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-27 23:47:50 +1000

    feat: add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import ErrorBoundary from "@components/errorBoundary/ErrorBoundary.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import NotFound from "@pages/notFound/NotFound.tsx";
 import About from "@pages/about/About.tsx";
+import Navbar from "@components/navbar/Navbar.tsx";
+import { ROUTES } from "@pages/routes.ts";
 
 export default function App() {
   return (
     <>
       <ErrorBoundary>
         <BrowserRouter>
+          <Navbar />
           <Routes>
             <Route path={ROUTES.Home()} element={<SearchPage />} />
             <Route path={ROUTES.About} element={<About />} />
