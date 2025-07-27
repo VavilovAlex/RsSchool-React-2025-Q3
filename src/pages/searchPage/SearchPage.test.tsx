865b9aa76b6aad1dc4361b0714f83a9080800cf4
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-07-28 01:26:34 +1000

    test: remove Test section check from SearchPage tests

--- src/pages/searchPage/SearchPage.test.tsx
+++ src/pages/searchPage/SearchPage.test.tsx
@@ -1,19 +1,18 @@
+import renderWithRouter from "@/test-utils/renderWithRouter.tsx";
 import { describe, it, expect } from "vitest";
 import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SearchPage from "./SearchPage";
-import renderWithRouter from "@/test-utils/renderWithRouter.tsx";
 
 const render = renderWithRouter;
 
 describe("SearchPage", () => {
-  it("renders Search, Results and Test sections", () => {
+  it("renders Search and Results sections", () => {
     render(<SearchPage />);
 
     expect(screen.getByRole("heading", { name: "Search" })).toBeInTheDocument();
     expect(
       screen.getByRole("heading", { name: "Results" }),
     ).toBeInTheDocument();
-    expect(screen.getByRole("heading", { name: "Test" })).toBeInTheDocument();
   });
 });
