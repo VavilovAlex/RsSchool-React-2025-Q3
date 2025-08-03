6d5022948e934b02f57ca83b0e5c224f0d45a8b7
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-08-03 23:42:54 +1000

    refactor: move popup to the right

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 export default function Popup({ isOpen, title, children }: Props) {
   return (
-    <div className="fixed bottom-0 right-0 w-full flex justify-center pointer-events-none">
+    <div className="fixed bottom-0 right-0 w-full flex justify-end pointer-events-none">
       <div
         data-testid="popup"
         className={clsx(
           "pointer-events-auto",
-          "p-4 bg-gray-100 mb-4 rounded",
+          "p-4 bg-gray-100 m-4 rounded",
           "transform transition-all duration-300 ease-out",
           isOpen ? "translate-y-0 opacity-100" : "translate-y-full opacity-0",
         )}
