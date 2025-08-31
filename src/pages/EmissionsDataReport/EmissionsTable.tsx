ddc0bd7468755217669345c1ec00aac032dcb58b
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-09-01 00:48:08 +1000

    feat: extract DataRow and use memo across EmissionsTable.tsx

--- src/pages/EmissionsDataReport/EmissionsTable.tsx
+++ src/pages/EmissionsDataReport/EmissionsTable.tsx
@@ -1,5 +1,5 @@
 import type { Country, Data } from "@/types/emissionTypes.ts";
-import { use, useState } from "react";
+import { memo, use, useCallback, useMemo, useState } from "react";
 import ContentCard from "@components/contentCard";
 import { sortCompare } from "@/utils/compare.ts";
 import SortableColumn from "@components/sortableColumn";
@@ -7,197 +7,234 @@ import type { SortState } from "@components/sortableColumn/sortTypes.ts";
 import Input from "@components/input";
 import Select from "@components/select";
 import { isoCodesByRegion } from "@shared/iso_codes_by_region.ts";
+import { DataRow } from "@pages/EmissionsDataReport/DataRow.tsx";
 
-type DataColumn = keyof Data;
+export type DataColumn = keyof Data;
 type CountrySortBy = "name" | "iso_code" | "population";
 
 const maxYear = 2023;
 const minYear = 1750;
 
-export const EmissionsTable = ({
-  dataPromise,
-}: {
-  dataPromise: Promise<Country[]>;
-}) => {
-  const countriesRaw = use(dataPromise);
+export const EmissionsTable = memo(
+  function EmissionsTable({
+    dataPromise,
+  }: {
+    dataPromise: Promise<Country[]>;
+  }) {
+    const countriesRaw = use(dataPromise);
 
-  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+    const [selectedCountry, setSelectedCountry] = useState<Country | null>(
+      null,
+    );
 
-  const [visibleColumns] = useState<DataColumn[]>([
-    "year",
-    "population",
-    "co2",
-    "co2_per_capita",
-  ]);
+    const [visibleColumns] = useState<DataColumn[]>([
+      "year",
+      "population",
+      "co2",
+      "co2_per_capita",
+    ]);
 
-  const [sortCountriesBy, setSortCountriesBy] = useState<
-    SortState<CountrySortBy>
-  >({
-    key: "name",
-    direction: "asc",
-  });
-  const [sortDataBy, setSortDataBy] = useState<SortState<DataColumn>>({
-    key: "year",
-    direction: "desc",
-  });
-
-  const selectedCountryData =
-    selectedCountry?.data
-      .slice()
-      .sort((l, r) =>
-        sortCompare(l[sortDataBy.key], r[sortDataBy.key], sortDataBy.direction),
-      ) ?? [];
-
-  const [countrySearch, setCountrySearch] = useState("");
-  const [regionFilter, setRegionFilter] = useState<
-    keyof typeof isoCodesByRegion | ""
-  >("");
-  const [yearFilter, setYearFilter] = useState<number>(maxYear);
-
-  const countries = countriesRaw
-    .slice()
-    .sort((l, r) => {
-      if (sortCountriesBy.key === "name") {
-        return sortCompare(l.name, r.name, sortCountriesBy.direction);
-      }
-      if (sortCountriesBy.key === "iso_code") {
-        return sortCompare(l.iso_code, r.iso_code, sortCountriesBy.direction);
-      }
-
-      if (sortCountriesBy.key === "population") {
-        return sortCompare(
-          l.data.find((d) => d.year == yearFilter)?.population,
-          r.data.find((d) => d.year == yearFilter)?.population,
-          sortCountriesBy.direction,
-        );
-      }
-
-      throw new Error("Invalid sort key");
-    })
-    .filter((c) => c.name.toLowerCase().includes(countrySearch.toLowerCase()))
-    .filter((c) => {
-      if (regionFilter === "") {
-        return true;
-      }
-      return isoCodesByRegion[regionFilter].includes(c.iso_code ?? "");
+    const [sortCountriesBy, setSortCountriesBy] = useState<
+      SortState<CountrySortBy>
+    >({
+      key: "name",
+      direction: "asc",
+    });
+    const [sortDataBy, setSortDataBy] = useState<SortState<DataColumn>>({
+      key: "year",
+      direction: "desc",
     });
 
-  const regions = Object.keys(isoCodesByRegion);
+    const selectedCountryData = useMemo(
+      () =>
+        selectedCountry?.data
+          .slice()
+          .sort((l, r) =>
+            sortCompare(
+              l[sortDataBy.key],
+              r[sortDataBy.key],
+              sortDataBy.direction,
+            ),
+          ) ?? [],
+      [selectedCountry?.data, sortDataBy.direction, sortDataBy.key],
+    );
 
-  return (
-    <div className={"flex flex-col gap-4 h-full overflow-hidden"}>
-      <ContentCard title={"Emissions Data"}>
-        <div className={"flex flex-row justify-start gap-2"}>
-          <Input
-            label={"Search by country name"}
-            className={"w-[200px]"}
-            value={countrySearch}
-            onChange={(e) => setCountrySearch(e.target.value)}
-          />
-          <Select
-            label={"Filter by region"}
-            className={"w-[200px]"}
-            value={regionFilter}
-            onChange={(e) =>
-              setRegionFilter(
-                e.target.value as keyof typeof isoCodesByRegion | "",
-              )
+    const [countrySearch, setCountrySearch] = useState("");
+    const [regionFilter, setRegionFilter] = useState<
+      keyof typeof isoCodesByRegion | ""
+    >("");
+    const [yearFilter, setYearFilter] = useState<number>(maxYear);
+
+    const filterCountries = useCallback(
+      (countries: Country[]) => {
+        return countries
+          .slice()
+          .sort((l, r) => {
+            if (sortCountriesBy.key === "name") {
+              return sortCompare(l.name, r.name, sortCountriesBy.direction);
             }
-          >
-            <option value="">All</option>
-            {regions.map((r) => (
-              <option key={r} value={r}>
-                {r}
-              </option>
-            ))}
-          </Select>
-          <Select
-            label={"Filter by year"}
-            className={"w-[200px]"}
-            value={yearFilter}
-            onChange={(e) => setYearFilter(Number(e.target.value))}
-          >
-            {Array.from(
-              { length: maxYear - minYear + 1 },
-              (_, i) => maxYear - i,
-            ).map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </Select>
-        </div>
-        <div className={"flex-1 min-h-0 overflow-auto"}>
-          <table className={"default-table cursor-pointer"}>
-            <thead>
-              <tr>
-                <SortableColumn
-                  activeSort={sortCountriesBy}
-                  sortKey={"name"}
-                  onSort={setSortCountriesBy}
-                >
-                  Country
-                </SortableColumn>
-                <SortableColumn
-                  activeSort={sortCountriesBy}
-                  sortKey={"iso_code"}
-                  onSort={setSortCountriesBy}
-                >
-                  ISO Code
-                </SortableColumn>
-                <SortableColumn
-                  activeSort={sortCountriesBy}
-                  sortKey={"population"}
-                  onSort={setSortCountriesBy}
-                >
-                  Population
-                </SortableColumn>
-              </tr>
-            </thead>
-            <tbody>
-              {countries.map((c) => (
-                <tr key={c.name} onClick={() => setSelectedCountry(c)}>
-                  <td>{c.name}</td>
-                  <td>{c.iso_code ?? "N/A"}</td>
-                  <td>
-                    {c.data.find((d) => d.year == yearFilter)?.population ??
-                      "N/A"}
-                  </td>
-                </tr>
+            if (sortCountriesBy.key === "iso_code") {
+              return sortCompare(
+                l.iso_code,
+                r.iso_code,
+                sortCountriesBy.direction,
+              );
+            }
+
+            if (sortCountriesBy.key === "population") {
+              return sortCompare(
+                l.data.find((d) => d.year == yearFilter)?.population,
+                r.data.find((d) => d.year == yearFilter)?.population,
+                sortCountriesBy.direction,
+              );
+            }
+
+            throw new Error("Invalid sort key");
+          })
+          .filter((c) =>
+            c.name.toLowerCase().includes(countrySearch.toLowerCase()),
+          )
+          .filter((c) => {
+            if (regionFilter === "") {
+              return true;
+            }
+            return isoCodesByRegion[regionFilter].includes(c.iso_code ?? "");
+          });
+      },
+      [
+        countrySearch,
+        regionFilter,
+        sortCountriesBy.direction,
+        sortCountriesBy.key,
+        yearFilter,
+      ],
+    );
+
+    const countries = useMemo(
+      () => filterCountries(countriesRaw),
+      [countriesRaw, filterCountries],
+    );
+
+    const regions = Object.keys(isoCodesByRegion);
+
+    return (
+      <div className={"flex flex-col gap-4 h-full overflow-hidden"}>
+        <ContentCard title={"Emissions Data"}>
+          <div className={"flex flex-row justify-start gap-2"}>
+            <Input
+              label={"Search by country name"}
+              className={"w-[200px]"}
+              value={countrySearch}
+              onChange={(e) => setCountrySearch(e.target.value)}
+            />
+            <Select
+              label={"Filter by region"}
+              className={"w-[200px]"}
+              value={regionFilter}
+              onChange={(e) =>
+                setRegionFilter(
+                  e.target.value as keyof typeof isoCodesByRegion | "",
+                )
+              }
+            >
+              <option value="">All</option>
+              {regions.map((r) => (
+                <option key={r} value={r}>
+                  {r}
+                </option>
               ))}
-            </tbody>
-          </table>
-        </div>
-      </ContentCard>
-      {selectedCountry && (
-        <ContentCard title={selectedCountry.name}>
-          <table className={"default-table"}>
-            <thead className={"cursor-pointer"}>
-              <tr>
-                {visibleColumns.map((col) => (
+            </Select>
+            <Select
+              label={"Filter by year"}
+              className={"w-[200px]"}
+              value={yearFilter}
+              onChange={(e) => setYearFilter(Number(e.target.value))}
+            >
+              {Array.from(
+                { length: maxYear - minYear + 1 },
+                (_, i) => maxYear - i,
+              ).map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className={"flex-1 min-h-0 overflow-auto"}>
+            <table className={"default-table cursor-pointer"}>
+              <thead>
+                <tr>
                   <SortableColumn
-                    key={col}
-                    sortKey={col}
-                    onSort={setSortDataBy}
-                    activeSort={sortDataBy}
+                    activeSort={sortCountriesBy}
+                    sortKey={"name"}
+                    onSort={setSortCountriesBy}
                   >
-                    {col}
+                    Country
                   </SortableColumn>
+                  <SortableColumn
+                    activeSort={sortCountriesBy}
+                    sortKey={"iso_code"}
+                    onSort={setSortCountriesBy}
+                  >
+                    ISO Code
+                  </SortableColumn>
+                  <SortableColumn
+                    activeSort={sortCountriesBy}
+                    sortKey={"population"}
+                    onSort={setSortCountriesBy}
+                  >
+                    Population
+                  </SortableColumn>
+                </tr>
+              </thead>
+              <tbody>
+                {countries.map((c) => (
+                  <tr key={c.name} onClick={() => setSelectedCountry(c)}>
+                    <td>{c.name}</td>
+                    <td>{c.iso_code ?? "N/A"}</td>
+                    <td>
+                      {c.data.find((d) => d.year == yearFilter)?.population ??
+                        "N/A"}
+                    </td>
+                  </tr>
                 ))}
-              </tr>
-            </thead>
-            <tbody>
-              {selectedCountryData.map((d) => (
-                <tr key={d.year}>
+              </tbody>
+            </table>
+          </div>
+        </ContentCard>
+        {selectedCountry && (
+          <ContentCard title={selectedCountry.name}>
+            <table className={"default-table"}>
+              <thead className={"cursor-pointer"}>
+                <tr>
                   {visibleColumns.map((col) => (
-                    <td key={col}>{d[col] ?? "N/A"}</td>
+                    <SortableColumn
+                      key={col}
+                      sortKey={col}
+                      onSort={setSortDataBy}
+                      activeSort={sortDataBy}
+                    >
+                      {col}
+                    </SortableColumn>
                   ))}
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </ContentCard>
-      )}
-    </div>
-  );
-};
+              </thead>
+              <tbody>
+                {selectedCountryData.map((d) => (
+                  <DataRow
+                    key={d.year}
+                    data={d}
+                    visibleColumns={visibleColumns}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </ContentCard>
+        )}
+      </div>
+    );
+  },
+  (old, props) => {
+    return old.dataPromise === props.dataPromise;
+  },
+);
